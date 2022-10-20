82d34c68dca09264fd4ea4381e5724e656c691e5
Aurindam Jana <aurindam.jana@slint-ui.com>
2022-10-20 10:27:29 +0200

    send data as a param

--- netlify/functions/get-price.js
+++ netlify/functions/get-price.js
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch'
 
-export const handler = async () => {
-  const POKE_API = 'https://sandbox-checkout.paddle.com/api/2.0/prices?product_ids=33479'
+export const handler = async (event, context) => {
+
+  console.log({event}, {context})
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = 'https://sandbox-checkout.paddle.com/api/2.0/prices?product_ids=' + eventBody.product_ids
 
   const response = await fetch(POKE_API)
   const data = await response.json()
