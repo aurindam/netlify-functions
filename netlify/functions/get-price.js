0f39b29b8ca23437daead43b7425554b449165eb
Aurindam Jana <aurindam.jana@slint-ui.com>
2022-10-20 04:04:11 +0200

    fix file name

--- netlify/functions/get-price.js
+++ netlify/functions/get-price.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 export const handler = async () => {
-  const POKE_API = 'https://httpbin.org/get'
+  const POKE_API = 'https://sandbox-checkout.paddle.com/api/2.0/prices?product_ids=33479'
 
   const response = await fetch(POKE_API)
   const data = await response.json()
